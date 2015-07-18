c535c6d124729e7fde3ac23c01950cd1c9963c8a
Jan Henning Thorsen <jhthorsen@cpan.org>
2015-07-18 08:19:40 +0200

    Released version 0.24
     - Mark t/connection-refused.t as TODO, since it fail because of i18n

--- lib/Mojo/Redis2.pm
+++ lib/Mojo/Redis2.pm
@@ -6,7 +6,7 @@ Mojo::Redis2 - Pure-Perl non-blocking I/O Redis driver
 
 =head1 VERSION
 
-0.23
+0.24
 
 =head1 DESCRIPTION
 
@@ -129,7 +129,7 @@ use Carp ();
 use constant DEBUG => $ENV{MOJO_REDIS_DEBUG} || 0;
 use constant DEFAULT_PORT => 6379;
 
-our $VERSION = '0.23';
+our $VERSION = '0.24';
 
 my $PROTOCOL_CLASS = do {
   my $class = $ENV{MOJO_REDIS_PROTOCOL}
