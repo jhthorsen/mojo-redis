f6801ba5108730a73cfcf7c9fc5cd5e6abdbf2e5
Jan Henning Thorsen <jhthorsen@cpan.org>
2016-06-04 13:09:24 +0200

    Released version 0.26
     - Fix missing POD and removed code that is not in use #18

--- lib/Mojo/Redis2.pm
+++ lib/Mojo/Redis2.pm
@@ -9,7 +9,7 @@ use Carp ();
 use constant DEBUG => $ENV{MOJO_REDIS_DEBUG} || 0;
 use constant DEFAULT_PORT => 6379;
 
-our $VERSION = '0.25';
+our $VERSION = '0.26';
 
 my $PROTOCOL_CLASS = do {
   my $class = $ENV{MOJO_REDIS_PROTOCOL}
@@ -333,7 +333,7 @@ Mojo::Redis2 - Pure-Perl non-blocking I/O Redis driver
 
 =head1 VERSION
 
-0.25
+0.26
 
 =head1 DESCRIPTION
 
