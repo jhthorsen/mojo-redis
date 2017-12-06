29497c7b6866d53c5bd382f22aebd98d5846bb53
Dan Book <grinnz@grinnz.com>
2017-12-05 21:04:28 -0500

    Released version 0.30

--- lib/Mojo/Redis2.pm
+++ lib/Mojo/Redis2.pm
@@ -10,7 +10,7 @@ use Carp ();
 use constant DEBUG => $ENV{MOJO_REDIS_DEBUG} || 0;
 use constant DEFAULT_PORT => 6379;
 
-our $VERSION = '0.29';
+our $VERSION = '0.30';
 
 my $PROTOCOL_CLASS = do {
   my $class = $ENV{MOJO_REDIS_PROTOCOL}
@@ -340,7 +340,7 @@ Mojo::Redis2 - Pure-Perl non-blocking I/O Redis driver
 
 =head1 VERSION
 
-0.29
+0.30
 
 =head1 DESCRIPTION
 
