327f8cd84dbe0f4cf17237e6a3c40be095ee8633
Jan Henning Thorsen <jhthorsen@cpan.org>
2018-08-02 15:37:35 +0800

    Released version 3.08
     - Add benchmark test #3
     - Fix cache() need to use Protocol::Redis because of binary data
     - Fix GEOPOS return value
     - Fix only decode response data if defined #19
     - Fix do not create new connection objects during global destruction
     - Improved return values in documentation

--- lib/Mojo/Redis.pm
+++ lib/Mojo/Redis.pm
@@ -8,7 +8,7 @@ use Mojo::Redis::Cursor;
 use Mojo::Redis::Database;
 use Mojo::Redis::PubSub;
 
-our $VERSION = '3.07';
+our $VERSION = '3.08';
 
 $ENV{MOJO_REDIS_URL} ||= 'redis://localhost:6379';
 
